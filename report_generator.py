da14b29fb5366fc50b4e3328cc521fa635707f61
dgs-small <lucas9667@hotmail.com>
2024-06-06 18:01:43 -0300

    Improved code structure and generating .TAB file

--- report_generator.py
+++ report_generator.py
@@ -15,12 +15,21 @@ def generate_report_files(report_filename, content):
     João Marcos Gatis Araújo Silva; joaomg.silva@ucsal.edu.br; (71)99900-9154
     Gabriel Pinheiro Pedreira; gabriel.pedreira@ucsal.edu.br; (71)98125-6355
     Lucas Moreno Dantas Santos; lucasmoreno.santos@ucsal.edu.br; (71)99670-7576\n"""
-    report_filename_with_extension = report_filename + '.lex'
 
-    # Cria o arquivo de relatório e escreve as informações
-    with open(report_filename_with_extension, 'w') as report_file:
+    # Cria o arquivo de relatório .LEX
+    lex_reportfile_name = report_filename + '.LEX'
+    with open(lex_reportfile_name, 'w') as report_file:
         report_file.write(f"{report_file_header}\n")
         report_file.write("Conteudo:\n")
         report_file.write(content)
 
-    print(f"Relatório gerado com sucesso: {report_filename_with_extension}")
+    print(f"Relatório gerado com sucesso: {lex_reportfile_name}")
+    
+    # Cria o arquivo de relatório .TAB
+    tab_reportfile_name = report_filename + '.TAB'
+    with open(tab_reportfile_name, 'w') as report_file_tab:
+        report_file_tab.write(f"{report_file_header}\n")
+        report_file_tab.write("Conteudo:\n")
+        report_file_tab.write(content)
+        
+    print(f"Relatório gerado com sucesso: {tab_reportfile_name}")
